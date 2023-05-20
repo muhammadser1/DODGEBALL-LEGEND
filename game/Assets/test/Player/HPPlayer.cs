c8c6b33cfa48f92bc9522cf30bc37fdadccfc576
muhammad_Sarahni <117571362+muhammadser1@users.noreply.github.com>
2023-05-20 13:26:56 +0300

    week 10

--- game/Assets/test/Player/HPPlayer.cs
+++ game/Assets/test/Player/HPPlayer.cs
@@ -6,12 +6,12 @@ public class HPPlayer : MonoBehaviour
 {
     public int maxHealth = 100;
     private int currentHealth;
-
+    public int hp1 = 0;
     public Slider healthSlider;
 
     private void Start()
     {
-        currentHealth = maxHealth;
+        currentHealth = maxHealth-10;
         UpdateHealthSlider();
     }
 
@@ -20,6 +20,38 @@ public class HPPlayer : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.U))
         {
             DecreaseHealth(10);
+        }
+     /*   hp1++;
+        if (hp1 >= 10000)
+        {
+            currentHealth += 10;
+            UpdateHealthSlider();
+        }*/
+        if (hp1 == 0)
+        {
+            UnityEngine.Debug.Log("Player x position: " + transform.position.x);
+            UnityEngine.Debug.Log("Player y position: " + transform.position.y);
+            UnityEngine.Debug.Log("Player z position: " + transform.position.z);
+
+        
+              if (transform.position.x >= -60 && transform.position.x <= -58)
+              {
+                  if (transform.position.y >= 0 && transform.position.y <= 2)
+                  {
+                      if (transform.position.z >= 0 && transform.position.z <= 2)
+                      {
+                          currentHealth += 10;
+                          UpdateHealthSlider();
+                          hp1++;
+                          GameObject obj = GameObject.Find("Cube (6)1");
+
+                          if (obj != null)
+                          {
+                              Destroy(obj);
+                          }
+                      }
+                  }
+              }
         }
     }
 
